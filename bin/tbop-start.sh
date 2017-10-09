81b6416b6ceb5076e8c4029d26ec3fe3eec3ecaf
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-10-09 02:44:47 -0500

    Pull Elixir Judge on start.

--- bin/tbop-start.sh
+++ bin/tbop-start.sh
@@ -3,6 +3,7 @@
 docker pull mrrusof/judge
 docker pull mrrusof/ruby-sandbox
 docker pull mrrusof/python-sandbox
+docker pull mrrusof/elixir-sandbox
 cd /opt/tbop/share && \
     docker-compose pull && \
     docker-compose up -d
